1a8f3e30509550f0326d3f12df69787cee038f64
JoaoBronze <jemq@gft.com>
2024-12-20 17:26:23 -0300

    teste
    teste

---
+++ src/main/java/bs/bare/example/BS_Bare/BsBareApplication.java
@@ -0,0 +1,13 @@
+package bs.bare.example.BS_Bare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsBareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsBareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bs/bare/example/BS_Bare/BsBareApplicationTests.java
@@ -0,0 +1,13 @@
+package bs.bare.example.BS_Bare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsBareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
